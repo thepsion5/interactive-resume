4b125a440f290e5cd98c482a7f353bf744fee689
Sean Mumford <IE21SSM@tn.gov>
2017-01-22 19:41:00 -0600

    committing skeleton application configured for webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var APP_DIR = path.resolve(__dirname, 'src/app');
+var BUILD_DIR = path.resolve(__dirname, 'src/public');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  }
+};
+
+module.exports = config;
