ea9bce65195c08361e9a6147421ad330d23e472e
Sean Mumford <sean.mumford@gmail.com>
2017-01-26 15:37:13 -0600

    adding API-centric action creators and applying API middleware

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,3 +1,10 @@
+import { CALL_API } from './middlewares/api.js';
+
+export const GENERIC_API_REQUEST = 'api';
+export const LOAD_RESUME = 'resume.load';
+export const LOAD_RESUME_SUCCESS = 'resume.load.success';
+export const LOAD_RESUME_FAILED = 'resume.load.failed';
+
 export const SHOW_SKILLSETS = 'skillsets.show';
 export const HIDE_SKILLSETS = 'skillsets.hide';
 
@@ -14,4 +21,19 @@ export function hideSkillsets()
 		type: HIDE_SKILLSETS
 	};
 }
-
+ 
+export function loadResume()
+{
+ 	return {
+ 		type: GENERIC_API_REQUEST,
+ 		meta: {
+ 			CALL_API: {
+ 				method: 'get',
+ 				path: './resume-data.json',
+ 				sendingType: LOAD_RESUME,
+ 				successType: LOAD_RESUME_SUCCESS,
+ 				failureType: LOAD_RESUME_FAILED
+ 			}
+ 		}
+ 	}
+}
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './containers/App.jsx';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+import Api from './middlewares/api.js';
+
+const store = createStore(reducer, applyMiddleware(Api));
 
 require("./stylesheet.css");
 
